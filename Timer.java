02f688b80041f0570fc9993bef5348dddbb08488
Julio Berina <julioberina@gmail.com>
2018-10-12 02:47:00 -0700

    Add Timer class

---
+++ Timer.java
@@ -0,0 +1,45 @@
+public class Timer
+{
+    private int size;
+    private long totalCost;
+    private long totalTime;
+    private long temp;
+    private int successCount;
+    
+    public Timing()
+    {
+        size=0;
+        totalCost=0;
+        totalTime=0;
+        temp=0;
+    }
+    public long getAveCost()
+    {
+        return totalCost/size;
+    }
+
+    public void startTime()
+    {
+        temp=System.nanoTime();
+    }
+
+    public void endTime()
+    {
+        totalTime += temp - System.nanoTime();
+    }
+
+    public long getAveTime()
+    {
+        return totalTime/size;
+    }
+
+    public double successRate()
+    {
+        return ((double)successCount/(double)size)*100;
+    }
+
+    public double successCount()
+    {
+        return successCount;
+    }
+}
